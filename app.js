fde55a068e3a859845b8e22a14ac4124afab2ebe
eric <eric.drake2011@gmail.com>
2022-11-07 20:48:52 -0600

    Update operate function to work properly

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const calcBody = document.getElementById('calcBody');
 const currentNumber = document.getElementById("currentNumber");
 const equationPreview = document.getElementById("equationPreview");
 const clrBtn = document.getElementById("clr");
 const delBtn = document.getElementById("del");
 const equal = document.getElementById("equal");
 const point = document.getElementById("point");
+const numbers = document.querySelectorAll(".numBtn");
+const operators = document.querySelectorAll(".opBtn");
+let resetReady = false;
+
 
 currentNumber.textContent = "0";
 
 clrBtn.addEventListener("click", clear);
 delBtn.addEventListener("click", backSpace);
-equal.addEventListener("click", operate);
+equal.addEventListener("click", () => operate(equal.textContent));
 point.addEventListener("click", displayPoint);
-
-const numbers = document.querySelectorAll(".numBtn");
 numbers.forEach((btn) => btn.addEventListener("click", () => displayCurrent(btn.textContent)));
+operators.forEach((btn) => btn.addEventListener("click", () => operate(btn.textContent)))
 
 // Operator functions:
 function add(a, b) {
@@ -33,15 +35,50 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b, sign) {
-    return sign === '+' ? add(a, b)
-        : sign === '-' ? subtract(a, b)
-        : sign === '*' ? multiply(a, b)
-        : divide(a, b);
+function operate(operator) {
+    if (equationPreview.textContent == '') {
+        if (resetReady && operator == '=') return
+
+        resetReady = false;
+        equationPreview.textContent = `${currentNumber.textContent} ${operator}`
+        currentNumber.textContent = '0';
+        return
+    } else {
+        let sign = equationPreview.textContent.slice(-1);
+        let a = Number(equationPreview.textContent
+                .slice(0, -1)
+                .trim());
+        let b = Number(currentNumber.textContent);
+
+        if (operator == '=') {
+            equationPreview.textContent = '';
+            resetReady = true;
+            currentNumber.textContent = (sign === '+') ? add(a, b)
+            : sign === '-' ? subtract(a, b)
+            : sign === 'x' ? multiply(a, b)
+            : divide(a, b);
+            return
+        }
+
+        equationPreview.textContent = (sign === '+') ? `${add(a, b)} ${operator}`
+            : sign === '-' ? `${subtract(a, b)} ${operator}`
+            : sign === 'x' ? `${multiply(a, b)} ${operator}`
+            : `${divide(a, b)} ${operator}`;
+        currentNumber.textContent = "0";
+    }
 }
 
 // Display functions:
 function displayCurrent(number) {
+    if (resetReady) {
+        clear();
+        resetReady = false;
+    }
+
+    if (currentNumber.textContent == '0'){
+        currentNumber.textContent = '';
+    }
+
     currentNumber.textContent += number;
 }
 
@@ -49,6 +86,7 @@ function displayPoint() {
     if (currentNumber.textContent == '') {
         currentNumber.textContent = '0';
     }
+
     if (currentNumber.textContent.includes('.')) return
     currentNumber.textContent += ".";
 }
@@ -61,5 +99,5 @@ function backSpace() {
 
 function clear() {
     equationPreview.textContent = "";
-    currentNumber.textContent = "";
+    currentNumber.textContent = "0";
 }
